dae65bc52b2faf25822b842758d64d7e59d64e29
Dave Fuller <dave@accentdesign.co.uk>
2017-02-06 22:01:08 +0000

    Addition of LESS tasks

--- README.md
+++ README.md
@@ -6,11 +6,16 @@ After cloning project run to install dependencies
 
 Run the required tasks on the command line 'e.g. gulp css-process-dev'
 
-- sass-process (process less to css)
+- sass-process (process sass to css)
+- less-process (process less to css)
 - css-compress (compress css file)
 - css-prefix (autoprefix css file)
 - css-sourcemap (addition of source map inline to css file to help debugging)
-- css-process-prod (for production runs following [sass-process,css-compress,css-prefix])
-- css-process-dev (for development runs following [sass-process,css-compress,css-prefix,css-sourcemap])
-- watch-dev (adds watcher to the main less file and runs css-process-dev task)
-- watch-prod (adds watcher to the main less file and runs css-process-prod task)
+- sass-process-prod (for production runs following [sass-process,css-compress,css-prefix])
+- sass-process-dev (for development runs following [sass-process,css-compress,css-prefix,css-sourcemap])
+- sass-watch-dev (adds watcher to the main sass file and runs sass-process-dev task)
+- sass-watch-prod (adds watcher to the main sass file and runs sass-process-prod task)
+- less-process-prod (for production runs following [less-process,css-compress,css-prefix])
+- less-process-dev (for development runs following [less-process,css-compress,css-prefix,css-sourcemap])
+- less-watch-dev (adds watcher to the main less file and runs less-process-dev task)
+- less-watch-prod (adds watcher to the main less file and runs less-process-prod task)
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var less = require('gulp-less'); // if you have to use less
 var cssnano = require('gulp-cssnano');
 var runSequence = require('run-sequence');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 
-
+// paths
 
 var basePath = {
     src: 'wwwroot/css/',
@@ -22,11 +23,12 @@ var paths = {
     styles: {
         src: basePath.src + files.src,
         allsass: basePath.src + '*.scss',
+        allless: basePath.src + '*.less',
         dest: basePath.dest + files.dest
     }
 }
 
-
+// tasks
 
 gulp.task('sass-process', function() {
     return gulp.src(paths.styles.src)
@@ -35,9 +37,16 @@ gulp.task('sass-process', function() {
         .pipe(gulp.dest(basePath.dest))
 });
 
+gulp.task('less-process', function() {
+    return gulp.src(paths.styles.src)
+        .pipe(less())
+        .pipe(concat(files.dest))
+        .pipe(gulp.dest(basePath.dest))
+});
+
 gulp.task('css-compress', function() {
     return gulp.src(paths.styles.dest)
-        .pipe(cssnano())
+        .pipe(cssmin())
         .pipe(concat(files.dest))
         .pipe(gulp.dest(basePath.dest))
 });
@@ -58,18 +67,39 @@ gulp.task('css-sourcemap', function() {
         .pipe(gulp.dest(basePath.dest))
 });
 
-gulp.task('css-process-prod', function(callback) {
+
+// SASS
+
+gulp.task('sass-process-prod', function(callback) {
     runSequence('sass-process', 'css-compress', 'css-prefix', callback);
 });
 
-gulp.task('css-process-dev', function(callback) {
+gulp.task('sass-process-dev', function(callback) {
     runSequence('sass-process', 'css-prefix', 'css-compress', 'css-sourcemap', callback);
 });
 
-gulp.task('watch-dev', function() {
-    gulp.watch(paths.styles.allsass,['css-process-dev']);
+gulp.task('sass-watch-dev', function() {
+    gulp.watch(paths.styles.allsass,['sass-process-dev']);
 });
 
-gulp.task('watch-prod', function() {
-    gulp.watch('paths.styles.src',['css-process-prod']);
+gulp.task('sass-watch-prod', function() {
+    gulp.watch(paths.styles.allsass,['sass-process-prod']);
+});
+
+// LESS
+
+gulp.task('less-process-prod', function(callback) {
+    runSequence('less-process', 'css-compress', 'css-prefix', callback);
+});
+
+gulp.task('less-process-dev', function(callback) {
+    runSequence('less-process', 'css-prefix', 'css-compress', 'css-sourcemap', callback);
+});
+
+gulp.task('less-watch-dev', function() {
+    gulp.watch(paths.styles.allless,['less-process-dev']);
+});
+
+gulp.task('less-watch-prod', function() {
+    gulp.watch(paths.styles.allless,['less-process-prod']);
 });
